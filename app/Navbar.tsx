eaae5cc4f8724125083cc8bd2a8df0a80ac66715
ryan-didevar <abdidevar@gmail.com>
2023-11-19 18:26:41 +0330

    Add Navbar and Header

---
+++ app/Navbar.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { ReactElement } from "react";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+const Navbar = () => {
+  const links: { label: string; href: string; icon?: ReactElement }[] = [
+    { label: "Home", href: "/", icon: <AiFillHome /> },
+    { label: "About", href: "/about", icon: <BsFillInfoCircleFill /> },
+  ];
+  return (
+    <nav className="flex justify-between items-center my-6 mx-2 sm:mx-auto max-w-6xl">
+      <div className="flex gap-1 lg:gap-2 items-center">
+        {links.map((link) => (
+          <Link
+            key={link.label}
+            href={link.href}
+            className="flex gap-2 items-center hover:text-amber-500"
+          >
+            <div className="inline sm:hidden text-2xl mx-4">{link.icon}</div>
+            <p className="sm:inline hidden text-sm my-2">
+              {link.label.toUpperCase()}
+            </p>
+          </Link>
+        ))}
+      </div>
+      <div>
+        <Link href="/">
+          <h2 className="flex gap-1 items-center">
+            <span className="px-2 py-1 bg-amber-500 rounded-lg font-bold ">
+              IMDb
+            </span>
+            <span className="font-semibold hidden sm:inline">Clone</span>
+          </h2>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
