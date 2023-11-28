b63bc194638634027676289de82d6de31bf24c86
ryan-didevar <abdidevar@gmail.com>
2023-11-29 02:55:58 +0330

    Add loading skeleton

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,7 +5,19 @@ const Loading = () => {
   ];
   return (
     <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
-      Hey
+      {moviesList.map((movie) => (
+        <div
+          key={movie}
+          className="h-50 p-4 cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group"
+        >
+          <Skeleton className="w-20" height="10rem" />
+          <div>
+            <Skeleton className="w-20" height="2rem" />
+            <Skeleton className="w-20" height="2rem" />
+            <Skeleton className="w-20" />
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
